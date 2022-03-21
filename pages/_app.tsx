72fc5f5ab21750a430488dda2a8c83236fd554bb
komenzar <takuya.kometan@gmail.com>
2022-03-21 23:51:40 +0900

    テスト｜Scriptの位置を変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,12 @@ import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+    <>
       <Script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?hQIULW9VvKs%3D&box=eAXUhX4aEKE%3D"></Script>
-    </ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
